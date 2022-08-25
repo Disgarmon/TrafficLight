3252c7c72975cdfa570e408ad06bd4aed41f226b
Daniil Lebedev <disgarmonvideo@gmail.com>
2022-08-25 14:33:27 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Daniil Lebedev on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
